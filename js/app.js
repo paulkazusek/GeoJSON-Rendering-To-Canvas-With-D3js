588e1f98e477d6e3f4421d0e8ac6feb28943c101
Paul Kazusek <51259999+paulkazusek@users.noreply.github.com>
2019-06-03 23:26:27 +0200

    add geo path and draw

--- js/app.js
+++ js/app.js
@@ -9,3 +9,23 @@ ctx.canvas.height = '400';
 var context = d3.select('#my-canvas')
     .node()
     .getContext('2d');
+
+var projection = d3.geoEquirectangular()
+    .scale(25000)
+    .translate([400, 200])
+    .center([7.145, 51.5175]);
+
+var geoGenerator = d3.geoPath()
+    .projection(projection)
+    .context(context);
+
+function update(geojson) {
+    context.lineWidth = 0.5;
+    context.strokeStyle = '#aaa';
+
+    context.beginPath();
+    geoGenerator({ type: 'FeatureCollection', features: geojson.features })
+    context.stroke();
+}
+
+update(geojson);
