6903492556b9917a1882fbea8309f470e2922821
Paul Kazusek <51259999+paulkazusek@users.noreply.github.com>
2019-06-03 22:04:15 +0200

    select the canvas element for d3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-// 
+// set widht and height
 var canvas = document.getElementById('my-canvas');
 var ctx = canvas.getContext('2d');
 
 ctx.canvas.width  = '800';
 ctx.canvas.height = '400';
+
+// 
+var context = d3.select('#my-canvas')
+    .node()
+    .getContext('2d');
