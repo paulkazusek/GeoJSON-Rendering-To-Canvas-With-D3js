0aeddab78c87980d743dbf6d379fc05e92ec6ebc
Paul Kazusek <51259999+paulkazusek@users.noreply.github.com>
2019-06-11 11:17:49 +0200

    read geojson from file

--- js/geojson.js
+++ js/geojson.js
@@ -1,1 +1,12 @@
-var geojson = [];
+function loadGeoJson(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'data/ruhr.geojson', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
