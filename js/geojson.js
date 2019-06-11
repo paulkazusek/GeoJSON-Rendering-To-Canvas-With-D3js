640ab8863fce6df7fc1697d7aad0bd9678cadf40
paul kazusek <paul.kazusek@gmail.com>
2019-06-11 11:46:34 +0200

    change file name

--- js/geojson.js
+++ js/geojson.js
@@ -1,7 +1,7 @@
 function loadGeoJson(callback) {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'data/ruhr.geojson', true);
+  xobj.open('GET', 'data/ruhr_area.geojson', true);
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
       // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
